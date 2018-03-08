f056af450ad501b6e0d20d231ab1a6ad18606506
kbperez <kbpgperez@hotmail.com>
2018-03-08 15:21:07 -0800

    added test

---
+++ __test__/auth.test.js
@@ -0,0 +1,8 @@
+import reducer from '../src/reducer/auth';
+require('jest');
+
+describe('auth reducer', () => {
+  it('should return the initial state on first call', () => {
+    expect((1+2)).toEqual(3);
+  });
+});
